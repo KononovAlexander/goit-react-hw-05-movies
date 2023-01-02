7765e98e1c18702f853cfb5352c9edc8e2d12075
unknown <kononov8214@gmail.com>
2023-01-02 16:44:29 +0300

    comments removed

--- src/components/api.js
+++ src/components/api.js
@@ -2,14 +2,8 @@ import axios from 'axios';
 
 const KEY = '805efc04c3a7e613b1259ae766adc4d8';
     axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
-    // const baseURL = 'https://api.themoviedb.org/3/';
 
     export const getSearchFilms = async (query) => {
-        // const films = await fetch(`${baseURL}/search/movie/?api_key=${KEY}&query=${query}`)
-        // .then(response => response.json())
-        // .then(data => {return data})
-        // console.log('films.results: ', films.results);
-        // if(films){return films.results}else{return {}}
 
         const films = await axios.get('search/movie', {
             params: {
@@ -22,10 +16,7 @@ const KEY = '805efc04c3a7e613b1259ae766adc4d8';
     }
 
     export const  getTrendFilms = async () => {
-            // const response = await fetch(`${baseURL}/trending/movie/week?api_key=${KEY}`)
-            // .then(response => response.json())
-            // .then(data => {return data})
-            // return response.results;
+
             const films = await axios.get('trending/movie/week', {
                 params: {
                   api_key: KEY,
@@ -35,11 +26,6 @@ const KEY = '805efc04c3a7e613b1259ae766adc4d8';
         }
 
     export const getFilmById = async (filmId) => {
-            
-            // const film = await fetch(`${baseURL}/movie/${filmId}?api_key=${KEY}`)
-            // .then(response => response.json())
-            // .then(data => {return data})
-            // return film;
 
             const film = await axios.get(`movie/${filmId}`, {
                 params: {
@@ -51,11 +37,6 @@ const KEY = '805efc04c3a7e613b1259ae766adc4d8';
         };
     export const getFilmCreditsByID = async (filmId) => {
 
-            // const credits = await fetch(`${baseURL}/movie/${filmId}/credits?api_key=${KEY}`)
-            // .then(response => response.json())
-            // .then(data => {return data})
-            // return credits;
-
             const credits = await axios.get(`movie/${filmId}/credits`, {
                 params: {
                   api_key: KEY,
@@ -65,10 +46,6 @@ const KEY = '805efc04c3a7e613b1259ae766adc4d8';
               return credits.data;
     };     
     export const getFilmReviewsByID = async (filmId) => {
-        // const reviews = await fetch(`${baseURL}/movie/${filmId}/reviews?api_key=${KEY}`)
-        // .then(response => response.json())
-        // .then(data => {return data})
-        // return reviews;
 
         const rewiews = await axios.get(`movie/${filmId}/reviews`, {
             params: {
